7e8e05870aa23eaa3f970fde0d8c3a5bf9b45b4a
Bibek Singh <bbek1717ingh@gmail.com>
2026-03-27 14:29:26 +0545

    backend part

---
+++ server/accounts/views.py
@@ -0,0 +1,31 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": "User created"})
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    user = authenticate(email=email, password=password)
+
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        })
+    else:
+        return Response({"error": "Invalid credentials"}, status=401)
